64940b8f963ed6544e4259a78a6367c316323945
valley <1518314680@qq.com>
2025-10-19 23:49:07 +0800

    init

---
+++ src/main/java/com/build/master/BuildMasterApiApplication.java
@@ -0,0 +1,11 @@
+package com.build.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildMasterApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BuildMasterApiApplication.class, args);
+    }
+}
